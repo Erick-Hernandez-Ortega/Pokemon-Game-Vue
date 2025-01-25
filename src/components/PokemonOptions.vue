4bc136f1a7515f1061fced38ca971871f5148bf3
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-25 16:22:38 -0600

    feat: crear componentes a utilizar

---
+++ src/components/PokemonOptions.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3>Soy pokemon options</h3>
+</template>
+
+<script>
+export default {
+    name: 'PokemonOptions'
+}
+</script>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>¿Quien es este pokemon?</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PokemonPage'
+}
+</script>
