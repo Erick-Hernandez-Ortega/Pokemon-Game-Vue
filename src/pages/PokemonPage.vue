3685718c7c42b8f7c2b22053b5019d389b06d63b
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-25 17:50:55 -0600

    feat: agregar componentes secundarios

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <h1>¿Quien es este pokemon?</h1>
+
+    <PokemonPicture :pokemon-id="5" />
+    <PokemonOptions />
   </div>
 </template>
 
 <script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
 export default {
-    name: 'PokemonPage'
+    name: 'PokemonPage',
+    components: {
+        PokemonOptions,
+        PokemonPicture
+    }
 }
 </script>
