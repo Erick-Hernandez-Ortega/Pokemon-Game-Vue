93030a2110a2a01313ceb0d3488eff37e900f980
Erick Ramón Hernández Ortega <erickramon47@live.com.mx>
2025-01-25 19:17:29 -0600

    feat: crear funciones para traernos los pokemones

---
+++ src/helpers/getPokemonOptions.js
@@ -0,0 +1,33 @@
+import pokemonAPI from "@/api/pokemonAPI";
+
+const getPokemons = () => {
+    const pokemons = Array.from(Array(650));
+
+    return pokemons.map((_, index) => index + 1)
+};
+
+const getPokemonNames = async(pokemons = []) => {
+    try {
+        const promises = pokemons.map((i) => pokemonAPI.get(`${i}`));
+        const responses = await Promise.all(promises)
+
+        return responses.map((pokemon) => {
+            return {
+                name: pokemon.data.name,
+                id: pokemon.data.id
+            }
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const getPokemonsOptions = async() => {
+    const mixPokemons = getPokemons().sort(() => Math.random() - 0.5)
+
+    const pokemons = await getPokemonNames(mixPokemons.slice(0, 4))
+    console.log(pokemons)
+    return pokemons
+}
+
+export default getPokemonsOptions;
